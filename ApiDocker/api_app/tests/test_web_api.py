49db4aca2edacc4ef4106bd3de6863be2508323d
Andrei <drang.andray@gmail.com>
2018-06-15 16:38:48 +0300

    Создание нового теста для проверки методов User модели. Правка старого теста для проверки работы url`ов с параметрами(добавление проверки возвращаемых ключей)

--- ApiDocker/api_app/tests/test_web_api.py
+++ ApiDocker/api_app/tests/test_web_api.py
@@ -1,23 +1,69 @@
 import requests
 
+from ApiDocker.api_app.web.database.models import User
 
-class TestUrls(object):
+
+class TestRouts(object):
     def setup_class(self):
         self.sess = requests.Session()
+        self.test_user_data = {'username': 'TestUsername',
+                               'password':'TestPass',
+                               'first_name':'TestFirstName',
+                               'last_name':'TestLastName',
+                               'email': 'test@test.com'}
 
-    # проверяем на доступность все возможные урлы
-    def test_web_api_routs(self):
-        doc = {"images": [{"href": "/images/1eaf6ef1-7f2d-4ecc-a8d5-6e8adba7cc0e.png"}]}
+    # проверяем корректность вывода информации о пользователе
+    def test_user_base_info(self):
+        # делаем запрос к api для получения информации о юзере
+        response = self.sess.get('http://localhost:8000/user_base_info/1')
+        # список ожидаемых ключей
+        default_keys = ['success','username', 'email', 'first_name', 'last_name', 'last_login_date', 'last_login_time']
 
-        routs = ['/received_data', '/search_data', '/send_mail', '/change_info', '/main']
-        for rout in routs:
-            response = self.sess.post(f'http://127.0.0.1:8000{rout}').json()
+        # проверка наличия всех нужных елчюей в ответе сервера
+        for key in response.json()['user'].keys():
+            assert key in default_keys
 
-            assert response == doc
+        assert response.status_code == 200
+        assert response.json()['user']['success']
 
-    # проверяем корректность ответа при неверном урле
-    def test_invalid_routs(self):
-        response = self.sess.post(f'http://127.0.0.1:8000/invalid_url').status_code
+    # проверяем корректность ответа при неверном id пользователя
+    def test_invalid_user_base_info(self):
+        response = self.sess.get(f'http://127.0.0.1:8000/user_base_info/0')
+        #
+        assert response.status_code == 404
+        assert not response.json()['user']['success']
+        assert response.json()['user']['error'] == 'no_user'
 
-        assert response == 404
 
+class TestDatabase(object):
+    def setup_class(self):
+        self.test_user_data = {'username': 'TestUsername',
+                               'password':'TestPass',
+                               'first_name':'TestFirstName',
+                               'last_name':'TestLastName',
+                               'email': 'test@test.com'}
+
+    # проверяем создание юзера
+    def test_new_user_creating(self):
+        new_user = User().create_new_user(new_user_data = self.test_user_data)
+
+        assert new_user['success']
+
+        # проверяем наличие юзера в бд
+        existed = User().check_user_exist(user_mail = self.test_user_data['email'],
+                                          username = self.test_user_data['username'])
+
+        assert existed['success']
+        assert existed['exist']
+
+        # удаляем юзера из бд
+        deleted = User().delete_user(user_id = new_user['user_id'])
+
+        assert deleted['success']
+
+        # проверяем отсутствие юзера в бд
+        existed = User().check_user_exist(user_mail = self.test_user_data['email'],
+                                          username = self.test_user_data['username'])
+
+        assert existed['success']
+        assert not existed['exist']
