fb3b44d76298a85fadc4e0344c49b2a4e77b7303
Andrei <drang.andray@gmail.com>
2018-07-30 01:01:51 +0300

    Создана заготовка формы для добавления новых данных к параметрам

--- WebDocker/WebApp/FitnessPersonalArea/forms.py
+++ WebDocker/WebApp/FitnessPersonalArea/forms.py
@@ -1,13 +1,21 @@
 from django.contrib.gis import forms
+from django.utils.translation import gettext_lazy as _
+
+from .models import BodyParameter
 
 
-# форма для создания нового зала
-class NewGym(forms.Form):
+# форма для внесения данных по параметрам пользователя
+class NewParameterData(forms.Form):
     # gym user name
-    gym_name = ''
+    user_parameter = forms.ChoiceField(label=_('Выберите параметр'),
+                                       widget = forms.Select(attrs = {'class': 'main-inp wide'}),
+                                       choices = [])
     # gym user description
-    gym_description = ''
-    # gym adress destination
-    gym_destination = ''
-    # gym geolocation
-    gym_geo = forms.PointField(widget=forms.OSMWidget(attrs={'map_width': 800, 'map_height': 500}))
+    body_data = ''
+
+    def __init__(self, *args, **kwargs):
+        super(NewParameterData, self).__init__(*args, **kwargs)
+        user_id = kwargs.pop('user_id', None)
+        # получаем список всех параметров выбранного пользователя
+        all_user_params = BodyParameter.objects.filter(user__user__id=user_id)
+        self.fields['user_parameter'].choices = ((x.id, x.body_title) for x in all_user_params)
